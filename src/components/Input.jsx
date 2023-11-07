a0616ff6b78a85c905227ecf9b6aae704d4c13ea
Laejun Kim <49670820+Laejun-Kim@users.noreply.github.com>
2023-11-07 13:50:50 +0900

    added some styles in Input.jsx

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { useState } from "react";
 import uuid from "react-uuid";
+import styled from "styled-components";
+
+const Stform = styled.form`
+  background-color: powderblue;
+  height: 100px;
+  text-align: center;
+  padding-top: 70px;
+`;
 
 function Input({ setTodo, toDos }) {
   const [title, setTitle] = useState("");
@@ -20,14 +28,14 @@ function Input({ setTodo, toDos }) {
     setContent(event.target.value);
   };
   return (
-    <form onSubmit={submitHndlr}>
+    <Stform onSubmit={submitHndlr}>
       <label>제목</label>
       <input required onChange={titleChangeHndlr} value={title} />
       <label>내용</label>
 
       <input onChange={contentChangeHndlr} value={content} />
       <button type="submit">등록하기</button>
-    </form>
+    </Stform>
   );
 }
 
